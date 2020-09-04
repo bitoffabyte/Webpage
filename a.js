72c4890b4c901e06e0ceee80be62da9235f52992
R Narayan <nr.rnarayan@gmail.com>
2020-09-05 00:03:03 +0530

    upcoming stuff

--- a.js
+++ a.js
@@ -5,16 +5,19 @@ const cont1 = document.querySelector('.cont1');
 const cont2 = document.querySelector('.cont2');
 const cont3 = document.querySelector('.cont3');
 left.addEventListener('mouseover', () => {
+    let opacity = 1;
     left.style.width = '100%';
     cent.style.width = '0%';
     right.style.width = '0%';
     cont1.style.opacity = 1;
+    document.querySelectorAll('.imgimg')[0].style.opacity = 0;
 });
 left.addEventListener('mouseout', () => {
     left.style.width = '33.333%';
     cent.style.width = '33.333%';
     right.style.width = '33.333%';
     cont1.style.opacity = 0;
+    document.querySelectorAll('.imgimg')[0].style.opacity = 1;
 });
 
 cent.addEventListener('mouseover', () => {
@@ -22,12 +25,14 @@ cent.addEventListener('mouseover', () => {
     left.style.width = '0%';
     right.style.width = '0%';
     cont2.style.opacity = 1;
+    document.querySelectorAll('.imgimg')[1].style.opacity = 0;
 });
 cent.addEventListener('mouseout', () => {
     cent.style.width = '33.333%';
     left.style.width = '33.333%';
     right.style.width = '33.333%';
     cont2.style.opacity = 0;
+    document.querySelectorAll('.imgimg')[1].style.opacity = 1;
 });
 
 right.addEventListener('mouseover', () => {
@@ -35,12 +40,14 @@ right.addEventListener('mouseover', () => {
     cent.style.width = '0%';
     left.style.width = '0%';
     cont3.style.opacity = 1;
+    document.querySelectorAll('.imgimg')[2].style.opacity = 0;
 });
 right.addEventListener('mouseout', () => {
     right.style.width = '33.333%';
     cent.style.width = '33.333%';
     left.style.width = '33.333%';
     cont3.style.opacity = 0;
+    document.querySelectorAll('.imgimg')[2].style.opacity = 1;
 });
 
 document.querySelector('.info1').onclick = () => {
@@ -68,3 +75,33 @@ document.querySelector('.close').onclick = () => {
 console.log(screen.height);
 document.querySelector('.pop').style.height = screen.height * 0.8 + 'px';
 document.querySelector('.pop').style.top = screen.height * 0.05 + 'px';
+
+document.querySelectorAll('.mask')[0].style.width =
+    document.querySelector('.iimmgg').offsetHeight +
+    document.querySelector('.iimmgg').offsetHeight * 0.66 +
+    'px';
+
+document.querySelectorAll('.mask')[0].style.left =
+    document.querySelector('.iimmgg').offsetLeft -
+    document.querySelector('.iimmgg').offsetLeft * 0.1 +
+    'px';
+
+document.querySelectorAll('.mask')[1].style.width =
+    document.querySelector('.iimmgg').offsetHeight +
+    document.querySelector('.iimmgg').offsetHeight * 0.66 +
+    'px';
+
+document.querySelectorAll('.mask')[1].style.left =
+    document.querySelector('.iimmgg').offsetLeft -
+    document.querySelector('.iimmgg').offsetLeft * 0.1 +
+    'px';
+
+document.querySelectorAll('.mask')[2].style.width =
+    document.querySelector('.iimmgg').offsetHeight +
+    document.querySelector('.iimmgg').offsetHeight * 0.66 +
+    'px';
+
+document.querySelectorAll('.mask')[2].style.left =
+    document.querySelector('.iimmgg').offsetLeft -
+    document.querySelector('.iimmgg').offsetLeft * 0.1 +
+    'px';
